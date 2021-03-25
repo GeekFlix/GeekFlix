ba151e8e27f333339d84b66a43673d7eb53409cf
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-03-25 20:42:28 +0100

    Add sideBar in home view

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,30 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+
+
 import './App.css';
+import Home from './home/home';
+
 
 import './css/main.css';
 
 function App() {
   return (
+
     <div className="App">
-      hola!
+      
+      <BrowserRouter>
+        <Navbar/>
+        <Switch>
+
+          <Route path='/' exact component={Home}/>
+
+        </Switch>
+      
+      </BrowserRouter>
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ app/src/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+import './home.css'
+
+const home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default home
