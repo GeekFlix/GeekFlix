5a3b749e8f8a08d884a321a0f7a1c8a72113aa4b
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-03-28 11:28:09 +0200

    Add state to loginModal

--- app/src/components/loginModal/loginModal.jsx
+++ app/src/components/loginModal/loginModal.jsx
@@ -2,30 +2,55 @@ import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import Input from '../input/input'
 
 
 const ModalLogin = (props) => {
   const {
-    buttonLabel,
-    className
+    classNameProps
   } = props;
 
   const [modal, setModal] = useState(false);
 
+  const [dataLogin,setLogin] = useState({
+    email : '',
+    password: ''
+  })
+
+  const handleState = (event) => {
+    setLogin({...dataLogin, [event.target.name]: event.target.type === "number" ? + event.target.value : event.target.value})
+  };
+  console.log(dataLogin)
+
   const toggle = () => setModal(!modal);
+
+  const sendLogin = async () => {
+    let result = await axios.post('http://localhost3000/')
+  }
 
   return (
     <div>
-      <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
-      <Modal isOpen={modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
-        toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
+      <Button className="btnSomething" onClick={toggle}>Iniciar Sesión</Button>
+      <Modal className="modal" isOpen={modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
+        toggle={toggle} className={classNameProps}>
+        <ModalHeader className="header modal-header" toggle={toggle}>Inicia Sesión
+        </ModalHeader>
+        <ModalBody className="body">
+        <div className="emailInput">
+            User:
+            <Input type="user" maxLength="50" onChange={handleState}/>
+          </div>
+          <div className="emailInput">
+            Email:
+            <Input type="email" maxLength="50" onChange={handleState}/>
+          </div>
+          <div className="paswordInput">
+            Password:
+            <Input type="password" maxLength="100" onChange={handleState}/>
+          </div>
         </ModalBody>
-        
-        <ModalFooter>
-          <Button className="btnSomething" onClick={toggle}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        <ModalFooter className="footer.modal">
+          <Button className="btnSomething" onClick={sendLogin()}>Iniciar Sesión</Button>
         </ModalFooter>
       </Modal>
     </div>
@@ -33,3 +58,5 @@ const ModalLogin = (props) => {
 }
 
 export default ModalLogin;
+
+
