e0b2cb7e8059fec453d76ff1bfbbae5adbfb7393
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-03-28 18:04:56 +0200

    finished view home

--- app/src/containers/home/home.jsx
+++ app/src/containers/home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import collageMovies from '../../assets/img/collageMovies.jpg';
-// import Button from '../../components/button/button';
+
 import ModalLogin from '../../components/loginModal/loginModal';
-import logo from "../../assets/img/geekflix-green.png";
+import logo from '../../assets/img/geekflix-green.png';
+import { Link } from 'react-router-dom';
 
 
 
@@ -20,13 +21,36 @@ const home = () => {
             <ModalLogin/>
           </div>
         </div>
+        <div className="textForRegister">
+          <div className="textRegisterTitle"><h1>Todas las películas y series que desees y mucho más...</h1></div>
+          <div className="textRegisterSubtitle"><h3>Disfruta donde quieras. Cancela cuando quieras.</h3></div>
+          <div className="textRegisterFinal"><h5>...Pero para todo ello inicie sesión o <Link to="/register" className="styleLink">SUSCRIBANSE!!</Link></h5></div>
+        </div>
         <img className="imgMovie" alt="collage" src={collageMovies}/>
+        
         <div className="joinTv">
           <div className="textPrincipalJoinTv">
             <div className="textTitleJoinTv">Disfruta de GeekFlix en tu TV.</div>
             <div className="textSecundaryJoinTv">Smart TV, Playstation, Xbox, Chromecast, Apple TV, reproductores Blu-ray y muchos más.</div>
           </div>
-          <div className="imageJoinTv">aqui va un video de youtube</div>
+          <div className="imageJoinTv">
+            <div className="videoTrailer">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/SAHnPVcmvTI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+
+        <div className="joinSeries">
+          <div className="imageJoinSeries">
+            <div className="seriesTrailer">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/GipNazWQuqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+          <div className="textPrincipalJoinSeries">
+            <div className="textTitleJoinSeries">Disfruta en todas partes de tus series favoritas.</div>
+            <div className="textSecundaryJoinSeries">Reproduce en streaming todas las películas y series en tu móvil, tableta, ordenador y TV sin pagar más.</div>
+          </div>
+          
         </div>
         
       </div>
