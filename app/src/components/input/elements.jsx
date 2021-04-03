23a630c45393cd786325323f1f010990292ce065
Laura <lauraporto596@gmail.com>
2021-04-03 22:42:53 +0200

    elements and input style

---
+++ app/src/components/input/elements.jsx
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import { FaRegUserCircle, FaRegTimesCircle } from "react-icons/fa";
+
+// const componentInput = () => {
+//     const ErrorLegend = () => {
+        
+//     }
+
+//     const ValidationIcon = () => {
+       
+//     }
+
+//     const ErrorMessage = () => {
+      
+//     }
+
+//     const SuccessMessage = () => {
+        
+//     }
+
+    // const Label = () => {
+
+    // }
+
+    // const FormComponent = () => {
+
+    // }
+
+    // const BtnContainer = () => {
+
+    // }
+
+//     return (
+//         <div>
+
+//         </div>
+//     )
+
+// }
+
+
+
+
+
+// export {
+//     ValidationIcon, 
+//     ErrorLegend, 
+//     ErrorMessage, 
+//     SuccessMessage, 
+//     Label, 
+//     FormComponent, 
+//     BtnContainer, 
+//     Input    
+    
+
+// }
--- app/src/components/input/input.jsx
+++ app/src/components/input/input.jsx
@@ -1,18 +1,18 @@
 // import React from 'react'; 
-// import {ErrorLegend, ValidationIcon, ErrorMessage, SuccessMessage} from '../elements/elements';
+// import {ErrorLegend, ValidationIcon, ErrorMessage, SuccessMessage, Label, FormComponent} from '../elements/elements';
 // import { FaRegUserCircle, FaRegTimesCircle } from "react-icons/fa";
 
-// const Input = ({state, handleState, type, label, placeholder, name, ErrorLegend, expresionRegular, ValidationIcon, FaRegTimesCircle, FaRegUserCircle}) => {
+// const InputComponent = ({state, type, label, placeholder, name, errorLegend, expresionRegular, ValidationIcon, FaRegTimesCircle, FaRegUserCircle, changeState, FormComponent}) => {
 // 	const onChange = (e) => {
-// 		handleState({...state, field: e.target.value});
+// 		changeState({...state, field: e.target.value});
 // 	}
 
 // 	const validation = () => {
 // 		if(expresionRegular){
 // 			if(expresionRegular.test(state.field)){
-// 				handleState({...state, valid: 'true'});
+// 				changeState({...state, valid: 'true'});
 // 			} else {
-// 				handleState({...state, valid: 'false'});
+// 				changeState({...state, valid: 'false'});
 // 			}
 // 		}
 
@@ -23,7 +23,7 @@
 
 // 	return (
 // 		<div>
-// 			<Label htmlFor={name} valid={state.valid}>{Label}</Label>
+// 			<Label htmlFor={name} valid={state.valid}>{label}</Label>
 // 			<InputGroup>
 // 				<Input 
 // 					type={type}
@@ -40,9 +40,48 @@
 // 					valid={state.valid}
 // 				/>
 // 			</InputGroup>
-// 			<ErrorLegend valid={state.valid}>{ErrorLegend}</ErrorLegend>
+// 			<ErrorLegend valid={state.valid}>{errorLegend}</ErrorLegend>
 // 		</div>
 // 	);
 // }
  
-// export default Input;
+// export default InputComponent;
+
+
+
+
+/*
+onKeyUp es una función que ejecuta una función en cuanto la tecla se levanta, leyendo así los cambios
+
+onBlur se ejecuta la función cuando das click fuera del input
+
+El if de la validación sería para repetir la contraseña, porque no haría falta la expresión regular, por lo que en este caso la condición sería innecesaria porque no hay input de repetición de contraseña. 
+
+expresionREgular = checkError?
+checkError(field.state)
+
+PENDIENTE:
+- funciones y extracción de props con sass --> si no metemos estilo a pelo en jsx
+- comprar expresiones regulares con archivo tools
+- comparar app.js con los lugares donde meteremos el formulario
+- create element para poder darle estilo a elements
+
+EN LOGIN, para que compruebe campo por campo del formulario (botón de enviar)
+
+const onSubmit = (e) => {
+    e.preventDefault();
+
+    if(email.valid === 'true' && password.valid === 'true'){
+        changeValid(true)
+        handleEmail({field:'', valid:null})
+        handleEmail({field: '', valid:null})
+    }else{
+        changeValid(false)
+    }
+}
+
+Esto se complementa llamando a formValid antes del elemento ErrorMessage {formValid === false && <ErrorMessage>}, de modo que si sale campos falsos salta el elemento del mensaje de error
+Procedemos de modo similar con el formulario de éxito (debajo del botón)
+{formValid === true && <SuccessMessage>}
+
+*/
